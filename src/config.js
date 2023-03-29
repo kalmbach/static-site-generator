50b2aba5e232edce7b6d000a968465254db23e2e
jorge <kalmbach@gmail.com>
2023-03-28 21:59:36 -0300

    move logic into src folder

--- src/config.js
+++ src/config.js
@@ -4,8 +4,9 @@ import { fileURLToPath } from "url";
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export const ASSETS_PATH = path.join(__dirname, "assets");
-export const BUILD_PATH = path.join(__dirname, "build");
 export const PAGES_PATH = path.join(__dirname, "pages");
-export const CONTENT_PATH = path.join(__dirname, "content");
+
+export const BUILD_PATH = path.join(__dirname, "../build");
+export const CONTENT_PATH = path.join(__dirname, "../content");
 
 export const POSTS_URI = "/posts";
