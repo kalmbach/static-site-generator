4a9d65aa1ac572573929649482c41980d8b9909a
jorge <kalmbach@gmail.com>
2023-03-29 11:51:52 -0300

    serve from BUILD_PATH config

--- src/server.js
+++ src/server.js
@@ -1,9 +1,10 @@
 import express from "express";
+import { BUILD_PATH } from "./config.js";
 
 const app = express();
 const port = 3000;
 
-app.use(express.static("build"));
+app.use(express.static(BUILD_PATH));
 
 app.listen(port, () => {
   console.log("Ready in http://localhost:3000");
